5c804641ea32d11befa00d00194ceea456939f02
Nico de Groot <ndegroot0@gmail.com>
2024-10-18 12:34:07 +0200

    oeps

--- firsttest.py
+++ firsttest.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-client = OpenAI(api_key='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',
+client = OpenAI(api_key='',
                 project='proj_9vUCFDiGw5IMcAQYjCsF92qP')
 
 audio_file = open("data/fragment.mp3", "rb")
