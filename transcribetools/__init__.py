88e6c58e913a040d06ea799b4e4a02c5f1190427
Nico de Groot <ndegroot0@gmail.com>
2025-07-15 22:56:27 +0200

    after cleanuo some filename changes, and settings

--- transcribetools/__init__.py
+++ transcribetools/__init__.py
@@ -1,5 +1,5 @@
-from .transcribe_folder import process_file, cli
+from transcribe_folder import process_file, cli
 
 __all__ = ["process_file", "cli"]
 
-__version__ = "0.5.8"  # It MUST match the version in pyproject.toml file
+__version__ = "0.5.9"  # It MUST match the version in the pyproject.toml file
